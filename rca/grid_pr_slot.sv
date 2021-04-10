e5d04ab94bf91992a584c1d6ea8b7bce13fb24ee
Aswin Vivekanandan <aswinvivekanandan@gmail.com>
2021-04-10 17:39:32 +0530

    add fifos to PR slot, add stub pr module

--- rca/grid_pr_slot.sv
+++ rca/grid_pr_slot.sv
@@ -18,6 +18,55 @@ module grid_pr_slot
     output data_valid_out
 );
 
-//TODO: expand to 2 inputs and FIFO them
+//It is anticipated that data at input 1 and input 2 will arrive at different rates => FIFOs are needed
+
+logic input1_fifo_populated;
+logic input2_fifo_populated;
+
+//FIFO to queue data from input 1
+    fifo_interface #(.DATA_WIDTH(XLEN) input1_fifo_if ();
+
+    taiga_fifo #(.DATA_WIDTH(XLEN), .FIFO_DEPTH(MAX_IDS)) input1_fifo (
+        .clk, .rst,
+        .fifo(input1_fifo_if)
+    );
+
+    assign input1_fifo_if.pop = ou_data_in_ack;
+    assign input1_fifo_if.data_in = data_in1;
+    assign input1_fifo_if.potential_push = data_valid_in1;
+    always_ff @(posedge clk) if (rst) input1_fifo_if.push <= 0;
+        else input1_fifo_if.push <= input1_fifo_if.potential_push;
+
+    assign input1_fifo_populated = input1_fifo_if.valid;
+
+    //FIFO to queue data from input 2
+    fifo_interface #(.DATA_WIDTH(XLEN) input2_fifo_if ();
+
+    taiga_fifo #(.DATA_WIDTH(XLEN), .FIFO_DEPTH(MAX_IDS)) input2_fifo (
+        .clk, .rst,
+        .fifo(input2_fifo_if)
+    );
+
+    assign input2_fifo_if.pop = ou_data_in_ack;
+    assign input2_fifo_if.data_in = data_in;
+    assign input2_fifo_if.potential_push = data_valid_in;
+    always_ff @(posedge clk) if (rst) input2_fifo_if.push <= 0;
+        else input2_fifo_if.push <= input2_fifo_if.potential_push;
+
+    assign input2_fifo_populated = input2_fifo_if.valid;
+
+    logic ou_data_in_ack;
+
+    pr_module ou(
+        .clk,
+        .rst,
+        .data_in1(input1_fifo_if.data_out),
+        .data_in2(input2_fifo_if.data_out),
+        .data_valid_in1(input1_fifo_populated),
+        .data_valid_in2(input2_fifo_populated),
+        .data_out,
+        .data_valid_out,
+        .data_in_ack(ou_data_in_ack)
+    )
     
 endmodule
---
+++ rca/pr_module.sv
@@ -0,0 +1,21 @@
+pr_module
+    import taiga_config::*;
+    import riscv_types::*;
+    import taiga_types::*;
+    import rca_config::*;    
+(
+    input clk,
+    input rst,
+
+    input [XLEN-1:0] data_in1,
+    input [XLEN-1:0] data_in2,
+
+    input data_valid_in1,
+    input data_valid_in2,
+
+    output [XLEN-1:0] data_out,
+    output data_valid_out,
+    output data_in_ack //just output high when the number of data valid signal for the input(s) this module requires is asserted
+);
+    
+endmodule
