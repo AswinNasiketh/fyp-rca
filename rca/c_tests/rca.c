978e7077babea9818971c87bd417f851c896b52b
Aswin Vivekanandan <aswinvivekanandan@gmail.com>
2021-04-13 23:21:15 +0530

    add C wrappers for assembly config and use instrs

---
+++ rca/c_tests/rca.c
@@ -0,0 +1,367 @@
+// Driver for Reconfigurable Custom Accelerators
+
+//Static Logic Configuration
+
+#include "rca.h"
+
+static void bad_config_trap(){
+    while(1);
+}
+
+static char rca_to_opcode_ext(rca_t rca){
+    switch (rca){
+        case RCA_A:
+            return 'a';
+        case RCA_B:
+            return 'b';
+        case RCA_C:
+            return 'c';
+        case RCA_D:
+            return 'd';
+    }
+}
+
+static inline void rca_a_config_cpu_reg(uint32_t rs1_val, uint32_t rs2_val){
+    uint32_t dummy;
+    asm volatile("rcaccr.a %0, %1, %2;"
+        : "=r"(dummy) //dummy output - instr does not write to rd
+        : "r"(rs1_val), "r"(rs2_val) //inputs
+        : // noclobbered registers
+    );
+}
+
+static inline void rca_b_config_cpu_reg(uint32_t rs1_val, uint32_t rs2_val){
+    uint32_t dummy;
+    asm volatile("rcaccr.b %0, %1, %2;"
+        : "=r"(dummy) //dummy output - instr does not write to rd
+        : "r"(rs1_val), "r"(rs2_val) //inputs
+        : // noclobbered registers
+    );
+}
+
+static inline void rca_c_config_cpu_reg(uint32_t rs1_val, uint32_t rs2_val){
+    uint32_t dummy;
+    asm volatile("rcaccr.c %0, %1, %2;"
+        : "=r"(dummy) //dummy output - instr does not write to rd
+        : "r"(rs1_val), "r"(rs2_val) //inputs
+        : // noclobbered registers
+    );
+}
+
+static inline void rca_d_config_cpu_reg(uint32_t rs1_val, uint32_t rs2_val){
+    uint32_t dummy;
+    asm volatile("rcaccr.d %0, %1, %2;"
+        : "=r"(dummy) //dummy output - instr does not write to rd
+        : "r"(rs1_val), "r"(rs2_val) //inputs
+        : // noclobbered registers
+    );
+}
+
+void rca_config_cpu_reg(rca_t rca, reg_port_t reg_port, reg_port_type_t reg_port_type, uint8_t reg_addr){
+
+    if(reg_addr > NUM_CPU_REGS-1){
+        bad_config_trap();
+    }
+
+
+    uint32_t rs1_val = (reg_port & REG_PORT_MASK) | 
+        ((reg_port_type << 3) & REG_PORT_TYPE_MASK);
+    
+    uint32_t rs2_val = reg_addr;
+
+    switch (rca) {
+        case RCA_A:
+            rca_a_config_cpu_reg(rs1_val, rs2_val);
+            break;
+        case RCA_B:
+            rca_b_config_cpu_reg(rs1_val, rs2_val);
+            break;
+        case RCA_C:
+            rca_c_config_cpu_reg(rs1_val, rs2_val);
+            break;
+        case RCA_D:
+            rca_d_config_cpu_reg(rs1_val, rs2_val);
+            break;
+    }
+
+}
+
+void rca_config_grid_mux(uint32_t mux_addr, uint32_t mux_sel){
+
+    if(mux_addr > NUM_GRID_MUXES-1){
+        bad_config_trap();
+    }
+
+    if(mux_sel > NUM_GRID_MUX_INPUTS-1){
+        bad_config_trap();
+    }
+
+    uint32_t rs1_val = mux_addr;
+    uint32_t rs2_val = mux_sel;
+    uint32_t dummy;
+
+    asm volatile("rcacgm %0, %1, %2;"
+        :"=r"(dummy)
+        :"r"(rs1_val), "r"(rs2_val)
+        :
+    );
+}
+
+void rca_config_io_mux(uint32_t io_unit_addr, uint32_t io_mux_sel){
+    if(io_unit_addr > NUM_IO_UNITS-1){
+        bad_config_trap();
+    }
+
+    if(io_mux_sel > NUM_IO_MUX_INPUTS-1){
+        bad_config_trap();
+    }
+
+    uint32_t rs1_val = io_unit_addr;
+    uint32_t rs2_val = io_mux_sel;
+    uint32_t dummy;
+
+    asm volatile("rcacim %0, %1, %2;"
+        :"=r"(dummy)
+        :"r"(rs1_val), "r"(rs2_val)
+        :
+    );
+}
+
+static void inline rca_a_config_result_mux(uint32_t rs1_val, uint32_t rs2_val){
+    uint32_t dummy;
+
+    asm volatile("rcacrm.a %0, %1, %2;"
+        :"=r"(dummy)
+        :"r"(rs1_val), "r"(rs2_val)
+        :
+    );
+}
+
+static void inline rca_b_config_result_mux(uint32_t rs1_val, uint32_t rs2_val){
+    uint32_t dummy;
+
+    asm volatile("rcacrm.b %0, %1, %2;"
+        :"=r"(dummy)
+        :"r"(rs1_val), "r"(rs2_val)
+        :
+    );
+}
+
+static void inline rca_c_config_result_mux(uint32_t rs1_val, uint32_t rs2_val){
+    uint32_t dummy;
+
+    asm volatile("rcacrm.c %0, %1, %2;"
+        :"=r"(dummy)
+        :"r"(rs1_val), "r"(rs2_val)
+        :
+    );
+}
+
+static void inline rca_d_config_result_mux(uint32_t rs1_val, uint32_t rs2_val){
+    uint32_t dummy;
+
+    asm volatile("rcacrm.d %0, %1, %2;"
+        :"=r"(dummy)
+        :"r"(rs1_val), "r"(rs2_val)
+        :
+    );
+}
+
+void rca_config_result_mux(rca_t rca, reg_port_t write_port, uint32_t io_unit_addr, bool fb_addr){
+    if(io_unit_addr > NUM_IO_UNITS-1){
+        bad_config_trap();
+    }
+
+    uint32_t rs1_val = write_port & REG_PORT_MASK;
+
+    if (fb_addr){
+        rs1_val |= 0x1 << 3;
+    }
+
+    uint32_t rs2_val = io_unit_addr;
+
+    switch (rca) {
+        case RCA_A:
+            rca_a_config_result_mux(rs1_val, rs2_val);
+            break;
+        case RCA_B:
+            rca_b_config_result_mux(rs1_val, rs2_val);
+            break;
+        case RCA_C:
+            rca_c_config_result_mux(rs1_val, rs2_val);
+            break;
+        case RCA_D:
+            rca_d_config_result_mux(rs1_val, rs2_val);
+            break;
+    }
+
+}
+
+static void inline rca_a_config_inp_io_unit_map(uint32_t rs1_val){
+    uint32_t dummy_result;
+    uint32_t dummy_input = 0;
+
+    asm volatile("rcacinp.a %0, %1, %2;"
+        :"=r"(dummy_result)
+        :"r"(rs1_val), "r"(dummy_result)
+        :
+    );
+}
+
+static void inline rca_b_config_inp_io_unit_map(uint32_t rs1_val){
+    uint32_t dummy_result;
+    uint32_t dummy_input = 0;
+
+    asm volatile("rcacinp.b %0, %1, %2;"
+        :"=r"(dummy_result)
+        :"r"(rs1_val), "r"(dummy_result)
+        :
+    );
+}
+
+static void inline rca_c_config_inp_io_unit_map(uint32_t rs1_val){
+    uint32_t dummy_result;
+    uint32_t dummy_input = 0;
+
+    asm volatile("rcacinp.c %0, %1, %2;"
+        :"=r"(dummy_result)
+        :"r"(rs1_val), "r"(dummy_result)
+        :
+    );
+}
+
+static void inline rca_d_config_inp_io_unit_map(uint32_t rs1_val){
+    uint32_t dummy_result;
+    uint32_t dummy_input = 0;
+
+    asm volatile("rcacinp.d %0, %1, %2;"
+        :"=r"(dummy_result)
+        :"r"(rs1_val), "r"(dummy_result)
+        :
+    );
+}
+
+//boolean array must be of size NUM_IO_UNITS
+void rca_config_inp_io_unit_map(rca_t rca, bool* io_unit_is_input){
+    uint32_t set_bit = 0x1;
+    uint32_t io_inp_map = 0;
+
+    for(int i = 0; i < NUM_IO_UNITS; i++){
+        if(io_unit_is_input[i]){
+            io_inp_map |= set_bit;
+        }
+        set_bit = set_bit << 1;
+    }
+
+    switch (rca)
+    {
+    case RCA_A:
+        rca_a_config_inp_io_unit_map(io_inp_map);
+        break;
+    case RCA_B:
+        rca_b_config_inp_io_unit_map(io_inp_map);
+        break;
+    case RCA_C:
+        rca_c_config_inp_io_unit_map(io_inp_map);
+        break;
+    case RCA_D:
+        rca_d_config_inp_io_unit_map(io_inp_map);
+    }
+}
+
+void rca_config_input_constant(uint32_t io_unit_addr, uint32_t c){
+    if(io_unit_addr > NUM_IO_UNITS-1){
+        bad_config_trap();
+    }
+
+    uint32_t dummy;
+
+    asm volatile("rcacic %0, %1, %2;"
+        :"=r"(dummy)
+        :"r"(io_unit_addr), "r"(c)
+        :
+    );
+}
+
+//RCA Usage
+
+inline void rca_a_use_fb(){
+    uint32_t dummy;
+
+    asm volatile("rcaufb.a %0, %0, %0"
+        :"+r"(dummy)
+        :
+        :
+    );
+}
+
+inline void rca_b_use_fb(){
+    uint32_t dummy;
+
+    asm volatile("rcaufb.b %0, %0, %0"
+        :"+r"(dummy)
+        :
+        :
+    );
+}
+
+inline void rca_c_use_fb(){
+    uint32_t dummy;
+
+    asm volatile("rcaufb.c %0, %0, %0"
+        :"+r"(dummy)
+        :
+        :
+    );
+}
+
+inline void rca_d_use_fb(){
+    uint32_t dummy;
+
+    asm volatile("rcaufb.d %0, %0, %0"
+        :"+r"(dummy)
+        :
+        :
+    );
+}
+
+inline void rca_a_use_nfb(){
+    uint32_t dummy;
+
+    asm volatile("rcaunfb.a %0, %0, %0"
+        :"+r"(dummy)
+        :
+        :
+    );
+}
+
+inline void rca_b_use_nfb(){
+    uint32_t dummy;
+
+    asm volatile("rcaunfb.b %0, %0, %0"
+        :"+r"(dummy)
+        :
+        :
+    );
+}
+
+inline void rca_c_use_nfb(){
+    uint32_t dummy;
+
+    asm volatile("rcaunfb.c %0, %0, %0"
+        :"+r"(dummy)
+        :
+        :
+    );
+}
+
+inline void rca_d_use_nfb(){
+    uint32_t dummy;
+
+    asm volatile("rcaunfb.d %0, %0, %0"
+        :"+r"(dummy)
+        :
+        :
+    );
+}
+
---
+++ rca/c_tests/rca.h
@@ -0,0 +1,63 @@
+#ifndef RCA_H
+#define RCA_H
+
+#include "stdbool.h"
+#include "stdint.h"
+#include "string.h"
+
+#define REG_PORT_MASK           0x00000007
+#define REG_PORT_TYPE_MASK      0x00000018
+#define NUM_CPU_REGS            32
+
+#define NUM_GRID_MUXES          144
+#define NUM_GRID_MUX_INPUTS     8
+
+#define NUM_IO_UNITS            14
+#define NUM_IO_MUX_INPUTS       12
+
+typedef enum {
+    RX1 = 0,
+    RX2 = 1,
+    RX3 = 2,
+    RX4 = 3,
+    RX5 = 4
+} reg_port_t;
+
+typedef enum {
+    SRC_PORT = 0b00,
+    NFB_DEST_PORT = 0b01,
+    FB_DEST_PORT = 0b11
+} reg_port_type_t;
+
+typedef enum {
+    RCA_A = 0,
+    RCA_B = 1,
+    RCA_C = 2,
+    RCA_D = 3
+} rca_t;
+
+static char rca_to_opcode_ext(rca_t rca);
+
+//Static Region Configuration
+void rca_config_cpu_reg(rca_t rca, reg_port_t reg_port, reg_port_type_t reg_port_type, uint8_t reg_addr);
+void rca_config_grid_mux(uint32_t mux_addr, uint32_t mux_sel);
+void rca_config_io_mux(uint32_t io_unit_addr, uint32_t io_mux_sel);
+void rca_config_result_mux(rca_t rca, reg_port_t write_port, uint32_t io_unit_addr, bool fb_addr);
+void rca_config_inp_io_unit_map(rca_t rca, bool* io_unit_is_input);
+void rca_config_input_constant(uint32_t io_unit_addr, uint32_t c);
+
+//RCA Use with feedback regs
+inline void rca_a_use_fb();
+inline void rca_b_use_fb();
+inline void rca_c_use_fb();
+inline void rca_d_use_fb();
+
+//RCA Use with non feedback regs
+inline void rca_a_use_nfb();
+inline void rca_b_use_nfb();
+inline void rca_c_use_nfb();
+inline void rca_d_use_nfb();
+
+
+
+#endif //RCA_H
