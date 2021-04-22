0a2ecee3f3a1256b9d0b72525dd140ea7fb44848
Aswin Vivekanandan <aswinvivekanandan@gmail.com>
2021-04-22 12:23:27 +0100

    add extra unused write port "IO unit"

--- rca/grid_wb.sv
+++ rca/grid_wb.sv
@@ -7,12 +7,26 @@ module grid_wb
     input [XLEN-1:0] io_unit_output_data [NUM_IO_UNITS],
     input io_unit_output_data_valid [NUM_IO_UNITS],
 
-    input [$clog2(NUM_IO_UNITS)-1:0] io_unit_sels [NUM_WRITE_PORTS],
+    input [$clog2(NUM_IO_UNITS+1)-1:0] io_unit_sels [NUM_WRITE_PORTS],
     input io_unit_sels_valid,
 
     output [XLEN-1:0] output_data [NUM_WRITE_PORTS],
     output wb_committing
 );
+
+    logic [XLEN-1:0] output_data [NUM_IO_UNITS + 1];
+    logic output_data_valid [NUM_IO_UNITS + 1];
+
+    always_comb begin
+        for(int i = 0; i < NUM_IO_UNITS; i++) begin
+            output_data[i] = io_unit_output_data[i];
+            output_data_valid[i] = io_unit_output_data_valid[i];
+        end
+    end
+
+    assign output_data[NUM_IO_UNITS] = 0;
+    assign output_data_valid[NUM_IO_UNITS] = 1; //always 1 to prevent port_ready_for_commit from being blocked by unused write ports
+    
 
     logic [NUM_WRITE_PORTS-1:0] port_ready_for_commit;
 
