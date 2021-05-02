89ac8f59e77eb1ea4f4f219308d315977806887c
Aswin Vivekanandan <anv17@ee-beholder0.ee.ic.ac.uk>
2021-05-02 19:18:13 +0100

    change default PR slot module to an empty one

---
+++ rca/pr_module_empty.sv
@@ -0,0 +1,37 @@
+module pr_module_empty
+    import taiga_config::*;
+    import riscv_types::*;
+    import taiga_types::*;
+    import rca_config::*;    
+(
+    input clk,
+    input rst,
+
+    input [XLEN-1:0] data_in1,
+    input [XLEN-1:0] data_in2,
+
+    input data_valid_in1,
+    input data_valid_in2,
+
+    output [XLEN-1:0] data_out,
+    output data_valid_out,
+    output data_in_ack1,
+    output data_in_ack2,
+
+    output uses_data_in1,
+    output uses_data_in2,
+
+    //LSQ interface
+    output [XLEN-1:0] addr, 
+    output [XLEN-1:0] data,
+    output [2:0] fn3,
+    output load,
+    output store,
+    output new_request,
+    input lsq_full,
+
+    input [XLEN-1:0] load_data,
+    input load_complete
+);
+
+endmodule
