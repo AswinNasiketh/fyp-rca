c1e978bf8d1f952bd1d2a9032595d83116d2a23e
Aswin Vivekanandan <anv17@ee-beholder0.ee.ic.ac.uk>
2021-05-02 20:14:46 +0100

    Modify make bb script to only set reconf property

--- rca/scripts/make_bb.tcl
+++ rca/scripts/make_bb.tcl
@@ -3,14 +3,11 @@
 
 open_checkpoint /home/anv17/FYP/fyp-rca/rca/dcps/top_no_bb.dcp
 
-for {set row 0} {$row < 12} {incr row} {
-	for {set col 0} {$col < 6} {incr col} {
-		set slot_name [format "pr_slots_row\[%u\].pr_slots_col\[%u\].grid_slot" $row $col]
-		set cell_name [get_cells -hierarchical $slot_name]
-		set_property HD.RECONFIGURABLE true $cell_name
-		update_design -cell $cell_name -black_box
-	}
+set rps [get_cells -hierarchical ou]
+
+foreach rp $rps {
+	set_property HD.RECONFIGURABLE true $rp
+#	update_design -cell $rp -black_box
 }
 
-#lock_design -level routing
 write_checkpoint /home/anv17/FYP/fyp-rca/rca/dcps/top_bb.dcp
