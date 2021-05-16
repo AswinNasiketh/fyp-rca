1b6d6de565f1ebd59b334be3a6aa16c58e87e68c
Aswin Vivekanandan <anv17@ee-beholder0.ee.ic.ac.uk>
2021-05-16 18:25:57 +0100

    Remove bootgen from PR scripts - do manually

--- rca/scripts/run_pr.tcl
+++ rca/scripts/run_pr.tcl
@@ -4,6 +4,3 @@ source /home/anv17/FYP/fyp-rca/rca/scripts/apply_pblocks_and_biggest_rm.tcl
 source /home/anv17/FYP/fyp-rca/rca/scripts/imp_rms.tcl
 source /home/anv17/FYP/fyp-rca/rca/scripts/verify_designs.tcl
 source  /home/anv17/FYP/fyp-rca/rca/scripts/gen_bitstreams.tcl
-
-exec "cd /home/anv17/FYP/fyp-rca/rca/bitstreams"
-exec "bootgen -image greybox.bif -arch zynq -process_bitstream bin -w on"
