8ceb7a2e335a76c2f3ebd406a23afce46428d394
Aswin Vivekanandan <aswinvivekanandan@gmail.com>
2021-05-24 00:51:41 +0100

    add dfg code

--- rca/sw/drivers/seqselect.c
+++ rca/sw/drivers/seqselect.c
@@ -32,6 +32,35 @@ void print_instr_seq(instr_seq_t seq){
     
 }
 
+void print_dfg(dfg_t dfg){
+    printf("Printing DFG \n\r");
+    printf("Nodes: \n\r");
+
+    dfg_node_t* curr_node = dfg.first_node;
+    while(curr_node != NULL){
+        printf("Node ID: %u, is_input: %u, is_output: %u, is_reg: %u, value: %u, op: %u\n\r",
+        curr_node->node_id,
+        curr_node->is_input,
+        curr_node->is_output,
+        curr_node->reg,
+        curr_node->inp_value,
+        curr_node->op
+        );
+        curr_node = curr_node->next_node;
+    }
+
+    printf("Edges: \n\r");
+    dfg_edge_t* curr_edge = dfg.first_edge;
+    while(curr_edge != NULL){
+        printf("Edge from: %u, to: %u, slot input: %u\n\r",
+        curr_edge->fromNode,
+        curr_edge->toNode,
+        curr_edge->slot_inp
+        );
+        curr_edge = curr_edge->next_edge;
+    }
+}
+
 void handle_profiler_exception(){
     toggle_profiler_lock();
     profiler_entry_t profiler_entries[NUM_PROFILER_ENTRIES];
@@ -54,6 +83,9 @@ void handle_profiler_exception(){
             prof_seqs[i].loop_branch_addr = profiler_entries[i].branch_addr;
             prof_seqs[i].seq_supported = analyse_instr_seq(profiler_entries[i].branch_addr, offset, &prof_seqs[i]);
             print_instr_seq(prof_seqs[i]);
+
+            dfg_t dfg = create_dfg(&prof_seqs[i]);
+            print_dfg(dfg);
         }
     }
 
@@ -169,3 +201,392 @@ void get_operands_s(uint32_t raw_instr, instr_t* instr){
     instr->input_operands[1].value = rs2;
 }
 
+void add_node(dfg_node_t* node_lst, dfg_node_t* node){
+    node_lst->next_node = node;
+}
+
+void init_node(dfg_node_t* node){
+    node->next_node = NULL;
+}
+
+void init_edge(dfg_edge_t* edge){
+    edge->next_edge = NULL;
+}
+
+void add_edge(dfg_edge_t* last_edge, dfg_edge_t* new_edge){
+    last_edge->next_edge = new_edge;
+}
+
+void init_new_node_reg(dfg_node_t* new_node, uint32_t reg_addr, uint32_t node_id){
+    init_node(new_node);
+    new_node->is_input = true;
+    new_node->reg = true;
+    new_node->inp_value = reg_addr;
+    new_node->node_id = node_id;
+    new_node->is_output = false;
+}
+
+void init_new_node_lit(dfg_node_t* new_node, uint32_t lit_val, uint32_t node_id){
+    init_node(new_node);
+    new_node->is_input = true;
+    new_node->reg = false;
+    new_node->inp_value = lit_val;
+    new_node->node_id = node_id;
+    new_node->is_output = false;
+}
+
+void init_new_node_op(dfg_node_t* new_node, ou_t op, uint32_t node_id){
+    init_node(new_node);
+    new_node->is_input = false;
+    new_node->op = op;
+    new_node->node_id = node_id;
+    new_node->is_output = false;
+}
+
+void init_new_edge(dfg_edge_t* new_edge, uint32_t fromNode, uint32_t toNode, grid_slot_inp_t slot_inp){
+    init_edge(new_edge);
+    new_edge->fromNode = fromNode;
+    new_edge->toNode = toNode;
+    new_edge->slot_inp = slot_inp;
+    // printf("New Edge - from %u, to %u\n\r", fromNode, toNode);
+}
+
+void init_new_node_output(dfg_node_t* new_node, uint32_t reg_addr, uint32_t node_id){
+    init_node(new_node);
+    new_node->is_input = false;
+    new_node->is_output = true;
+    new_node->reg = true; //always true for outputs
+    new_node->inp_value = reg_addr;
+    new_node->node_id = node_id;
+}
+
+void process_instr_inputs_normal(dfg_node_t** last_node, instr_t curr_instr, int32_t reg_last_write[NUM_CPU_REGS], dfg_edge_t** last_edge){
+    dfg_node_t* new_node;
+    dfg_edge_t* new_edge;
+    dfg_node_t* curr_op_node = *last_node;
+    // printf("Normal process instr inputs\n\r");
+    for(int i = 0; i < 2; i++){
+        if(curr_instr.input_operands[i].reg){
+            if(curr_instr.input_operands[i].value >= 0){
+                // printf("Reg Addr: %u\n\r", curr_instr.input_operands[i].value);
+                // printf("Reg Last Write: %d\n\r", reg_last_write[curr_instr.input_operands[i].value]);
+                if(reg_last_write[curr_instr.input_operands[i].value] == -1){                    
+                    new_node = malloc(sizeof(dfg_node_t));
+                    init_new_node_reg(new_node, curr_instr.input_operands[i].value, (*last_node)->node_id + 1);
+                    add_node(*last_node, new_node);
+                    *last_node = new_node;
+
+                    new_edge = malloc(sizeof(dfg_edge_t));
+                    init_new_edge(new_edge, new_node->node_id, curr_op_node->node_id, i);
+                    add_edge(*last_edge, new_edge);
+                    *last_edge = new_edge;
+                }else{
+                    uint32_t inp_node_id = reg_last_write[curr_instr.input_operands[i].value];
+
+                    new_edge = malloc(sizeof(dfg_edge_t));
+                    init_new_edge(new_edge, inp_node_id, curr_op_node->node_id, i);
+                    
+                    add_edge(*last_edge, new_edge);
+                    *last_edge = new_edge;
+                }
+            }
+        }else{
+            new_node = malloc(sizeof(dfg_node_t));
+            init_new_node_lit(new_node, curr_instr.input_operands[i].value, (*last_node)->node_id + 1);
+            add_node(*last_node, new_node);
+            *last_node = new_node;
+
+            new_edge = malloc(sizeof(dfg_edge_t));
+            init_new_edge(new_edge, new_node->node_id, curr_op_node->node_id, i);
+            add_edge(*last_edge, new_edge);
+            *last_edge = new_edge;
+        }
+    }
+}
+
+//NOTE: Whole AUIPC instruction could be replaced with a constant input
+void process_instr_inputs_auipc(dfg_node_t** last_node, instr_t curr_instr, int32_t reg_last_write[NUM_CPU_REGS], dfg_edge_t** last_edge, uint32_t instr_addr){
+    dfg_node_t* curr_op_node = *last_node;
+
+    //create literal node for current PC
+    dfg_node_t* new_node = malloc(sizeof(dfg_node_t));
+    init_new_node_lit(new_node, instr_addr, (*last_node)->node_id+1);
+    add_node(*last_node, new_node);
+    *last_node = new_node;
+
+    //add PC literal node as input 2 to current instr
+    dfg_edge_t* new_edge = malloc(sizeof(dfg_edge_t));
+    init_new_edge(new_edge, new_node->node_id, curr_op_node->node_id, INP2);
+    add_edge(*last_edge, new_edge);
+    *last_edge = new_edge;
+
+    //create literal node for upper immediate
+    new_node = malloc(sizeof(dfg_node_t));
+    init_new_node_lit(new_node, curr_instr.input_operands[0].value, (*last_node)->node_id+1);
+    add_node(*last_node, new_node);
+    *last_node = new_node;
+    
+    //add upper immediate as input 1 to current instr
+    new_edge = malloc(sizeof(dfg_edge_t));
+    init_new_edge(new_edge, new_node->node_id, curr_op_node->node_id, INP1);
+    add_edge(*last_edge, new_edge);
+    *last_edge = new_edge;
+}
+
+void process_instr_inputs_load(dfg_node_t** last_node, instr_t curr_instr, int32_t reg_last_write[NUM_CPU_REGS], dfg_edge_t** last_edge){
+    dfg_node_t* curr_op_node = *last_node;
+
+    //create addition structure to perform offset calculation to address
+    dfg_node_t* adder_node = malloc(sizeof(dfg_node_t));
+    init_new_node_op(adder_node, ADD, (*last_node)->node_id+1);
+    add_node(*last_node, adder_node);
+    *last_node = adder_node;
+
+
+    dfg_node_t* new_node;
+    dfg_edge_t* new_edge;
+
+    if(reg_last_write[curr_instr.input_operands[0].value] == -1){
+        new_node = malloc(sizeof(dfg_node_t));
+        init_new_node_reg(new_node, curr_instr.input_operands[0].value, (*last_node)->node_id+1);
+        add_node(*last_node, new_node);
+        *last_node = new_node;
+
+        new_edge = malloc(sizeof(dfg_edge_t));
+        init_new_edge(new_edge, new_node->node_id, adder_node->node_id, INP1);
+        add_edge(*last_edge, new_edge);
+        *last_edge = new_edge;
+    }else{
+        new_edge = malloc(sizeof(dfg_edge_t));
+        init_new_edge(new_edge, reg_last_write[curr_instr.input_operands[0].value], adder_node->node_id, INP1);
+        add_edge(*last_edge, new_edge);
+        *last_edge = new_edge;
+    }
+
+    new_node = malloc(sizeof(dfg_node_t));
+    init_new_node_lit(new_node, curr_instr.input_operands[1].value, (*last_node)->node_id+1);
+    add_node(*last_node, new_node);
+    *last_node = new_node;
+
+    new_edge = malloc(sizeof(dfg_edge_t));
+    init_new_edge(new_edge, new_node->node_id, adder_node->node_id, INP2);
+    add_edge(*last_edge, new_edge);
+    *last_edge = new_edge; 
+
+    //route adder output to load store unit
+
+    new_edge = malloc(sizeof(dfg_edge_t));
+    init_new_edge(new_edge, adder_node->node_id, curr_op_node->node_id, INP1);
+    add_edge(*last_edge, new_edge);
+    *last_edge = new_edge; 
+}
+
+void process_instr_inputs_store(dfg_node_t** last_node, instr_t curr_instr, int32_t reg_last_write[NUM_CPU_REGS], dfg_edge_t** last_edge){
+    dfg_node_t* curr_op_node = *last_node;
+
+    //create addition structure to perform offset calculation to address
+    dfg_node_t* adder_node = malloc(sizeof(dfg_node_t));
+    init_new_node_op(adder_node, ADD, (*last_node)->node_id+1);
+    add_node(*last_node, adder_node);
+    *last_node = adder_node;
+
+
+    dfg_node_t* new_node;
+    dfg_edge_t* new_edge;
+
+    if(reg_last_write[curr_instr.rd_addr] == -1){
+        new_node = malloc(sizeof(dfg_node_t));
+        init_new_node_reg(new_node, curr_instr.rd_addr, (*last_node)->node_id+1);
+        add_node(*last_node, new_node);
+        *last_node = new_node;
+
+        new_edge = malloc(sizeof(dfg_edge_t));
+        init_new_edge(new_edge, new_node->node_id, adder_node->node_id, INP1);
+        add_edge(*last_edge, new_edge);
+        *last_edge = new_edge;
+    }else{
+        new_edge = malloc(sizeof(dfg_edge_t));
+        init_new_edge(new_edge, reg_last_write[curr_instr.rd_addr] , adder_node->node_id, INP1);
+        add_edge(*last_edge, new_edge);
+        *last_edge = new_edge;
+    }
+
+    new_node = malloc(sizeof(dfg_node_t));
+    init_new_node_lit(new_node, curr_instr.input_operands[0].value, (*last_node)->node_id+1);
+    add_node(*last_node, new_node);
+    *last_node = new_node;
+
+    new_edge = malloc(sizeof(dfg_edge_t));
+    init_new_edge(new_edge, new_node->node_id, adder_node->node_id, INP2);
+    add_edge(*last_edge, new_edge);
+    *last_edge = new_edge;
+
+     //route adder output to load store unit
+
+    new_edge = malloc(sizeof(dfg_edge_t));
+    init_new_edge(new_edge, adder_node->node_id, curr_op_node->node_id, INP1);
+    add_edge(*last_edge, new_edge);
+    *last_edge = new_edge; 
+
+
+    //route data to store unit
+    if(reg_last_write[curr_instr.input_operands[1].value] == -1){
+        new_node = malloc(sizeof(dfg_node_t));
+        init_new_node_reg(new_node,curr_instr.input_operands[1].value, (*last_node)->node_id+1);
+        add_node(*last_node, new_node);
+        *last_node = new_node;
+
+        new_edge = malloc(sizeof(dfg_edge_t));
+        init_new_edge(new_edge, new_node->node_id, curr_op_node->node_id, INP2);
+        add_edge(*last_edge, new_edge);
+        *last_edge = new_edge;
+    }else{
+        new_edge = malloc(sizeof(dfg_edge_t));
+        init_new_edge(new_edge, reg_last_write[curr_instr.input_operands[1].value] , adder_node->node_id, INP1);
+        add_edge(*last_edge, new_edge);
+        *last_edge = new_edge;
+    }
+}
+
+
+void process_instr_inputs(dfg_node_t** last_node, instr_t curr_instr, int32_t reg_last_write[NUM_CPU_REGS], dfg_edge_t** last_edge, uint32_t curr_instr_addr){
+    bool is_load = false;
+    bool is_store = false;
+
+    const ou_t load_ous[] = {
+        LB,
+        LBU,
+        LH,
+        LHU,
+        LW,
+    };
+
+    const ou_t store_ous[] = {
+        SB,
+        SH,
+        SW
+    };
+
+    for(int i = 0; i < 5; i++){
+        if(curr_instr.ou == load_ous[i]){
+            is_load = true;
+            break;
+        }
+    }
+
+    for(int i = 0; i < 3; i++){
+        if(curr_instr.ou == store_ous[i]){
+            is_store = true;
+            break;
+        }
+    }
+
+    if(is_load){
+        process_instr_inputs_load(last_node, curr_instr, reg_last_write, last_edge);
+    }else if(is_store){
+        process_instr_inputs_store(last_node, curr_instr, reg_last_write, last_edge);
+    }else if(curr_instr.ou == AUIPC){
+        process_instr_inputs_auipc(last_node, curr_instr, reg_last_write, last_edge, curr_instr_addr);
+    }else{
+        process_instr_inputs_normal(last_node, curr_instr, reg_last_write, last_edge);
+    }    
+}
+
+dfg_t create_dfg(instr_seq_t* instr_seq){
+    dfg_t dfg;
+    int32_t reg_last_write[NUM_CPU_REGS];
+    for(int i = 0; i < NUM_CPU_REGS; i++){
+        reg_last_write[i] = -1;
+    }
+
+    instr_t curr_instr;
+
+    dfg_node_t firstNode;
+    dfg_edge_t firstEdge;
+    firstNode.node_id = 0;
+
+    dfg_node_t* lastNode = &firstNode;
+    dfg_edge_t* lastEdge = &firstEdge;
+    dfg_node_t* newNode;
+    dfg_edge_t* newEdge;
+
+    for(int i = 0, j = instr_seq->loop_start_addr; i < instr_seq->num_instrs, j < instr_seq->loop_branch_addr; i++, j+=4){
+        newNode = malloc(sizeof(dfg_node_t));
+        init_new_node_op(newNode, instr_seq->instr_arr[i].ou, lastNode->node_id+1);
+        add_node(lastNode, newNode);
+        lastNode = newNode;
+        
+        process_instr_inputs(&lastNode, instr_seq->instr_arr[i], reg_last_write, &lastEdge, j);
+        if(instr_seq->instr_arr[i].ou != SW && instr_seq->instr_arr[i].ou != SH && instr_seq->instr_arr[i].ou != SB){
+            reg_last_write[instr_seq->instr_arr[i].rd_addr] = newNode->node_id;
+        }
+    }
+
+
+    uint32_t finalNodeID = lastNode->node_id;
+    bool* hasOutputEdge = malloc(finalNodeID*sizeof(bool));
+
+    //if there is no output edge for the node, and it is an op, and it has a corresponding entry in the reg_last_write table
+
+
+    dfg_edge_t* curr_edge_ptr = firstEdge.next_edge;
+    while(curr_edge_ptr != NULL){
+        hasOutputEdge[curr_edge_ptr->fromNode] = true;
+        curr_edge_ptr = curr_edge_ptr->next_edge;
+    }
+    for(int i = 0; i < finalNodeID; i++){
+        if(hasOutputEdge[i]){
+            continue;
+        }else{
+            int32_t unwrittenReg = -1;
+            for(int j = 0; j < NUM_CPU_REGS; j++){
+                if(reg_last_write[j] == i){
+                    unwrittenReg = j;
+                    break;
+                }
+            }
+
+            if(unwrittenReg == -1){
+                continue; //can assume this is a store instruction
+            }else{
+
+                int32_t reg_id = -1;
+                //find node id of reg
+            
+                dfg_node_t* curr_node_ptr = &firstNode;
+
+                while(reg_id == -1 && curr_node_ptr != NULL){
+                    if(curr_node_ptr->is_input && curr_node_ptr->reg && curr_node_ptr->inp_value == unwrittenReg){
+                        reg_id = curr_node_ptr->node_id;
+                        curr_node_ptr->is_output = true;
+                    }else{
+                        curr_node_ptr = curr_node_ptr->next_node;
+                    }
+                }
+
+                //if register doesn't already have a node, create one
+                if(reg_id == -1){
+                    newNode = malloc(sizeof(dfg_node_t));
+                    init_new_node_output(newNode, i, lastNode->node_id+1);
+                    add_node(lastNode, newNode);
+                    lastNode = newNode;
+
+                    reg_id = newNode->node_id;
+                }
+
+                //create edge from output to register
+                newEdge = malloc(sizeof(dfg_node_t));
+                init_new_edge(newEdge, i, reg_id, INP1); //INP doesn't matter for output nodes
+                add_edge(lastEdge, newEdge);
+                lastEdge = newEdge;
+            }
+        }
+    }
+
+    dfg.first_edge = firstEdge.next_edge;
+    dfg.first_node = firstNode.next_node;
+
+    free(hasOutputEdge);
+
+    return dfg;
+}
--- rca/sw/drivers/seqselect.h
+++ rca/sw/drivers/seqselect.h
@@ -2,9 +2,11 @@
 #define SEQSELECT_H
 
 #include "rca.h"
+#include "static_region.h"
 #include <stdlib.h>
 
-#define SEQ_PROFILE_THRESH 20
+#define SEQ_PROFILE_THRESH          20
+#define MAX_ANNEALING_ATTEMPTS      5
 
 // void handle_profiler_exception(); // forward declared in board_support.h
 
@@ -28,11 +30,68 @@ typedef struct{
     bool seq_supported;
 }instr_seq_t;
 
+
+typedef struct dfg_node_t dfg_node_t;
+
+struct dfg_node_t{
+    bool is_input;
+    bool is_output;
+
+    bool reg;
+    uint32_t inp_value; //badly named
+    //When is_input and reg are true, this corresponds to register address. Otherwise if is_input is only true, then is is the literal value.
+    //If is_output is true, this corresponds to register address.
+
+    ou_t op;    
+
+    uint32_t node_id;
+
+    dfg_node_t* next_node; //linked list
+};
+
+typedef struct dfg_edge_t dfg_edge_t;
+
+struct dfg_edge_t{
+    uint32_t fromNode;
+    uint32_t toNode;
+    grid_slot_inp_t slot_inp;
+    
+    dfg_edge_t* next_edge; //linked list
+};
+
+typedef struct{
+    dfg_edge_t* first_edge;
+    dfg_node_t* first_node;
+}dfg_t;
+
+typedef struct{
+
+}pr_grid_t;
+
+typedef struct{
+    uint32_t cpu_time;
+    uint32_t acc_time; 
+    uint32_t reconf_time;
+
+    uint32_t init_interval;
+    uint32_t num_rows;
+
+    uint32_t num_annealing_attempts;
+
+    dfg_t* dfg;
+    pr_grid_t* pr_grid;
+}seq_profile_t;
+
+//INSTRUCION ID
 bool analyse_instr_seq(uint32_t branch_addr, int32_t branch_offset, instr_seq_t* instr_seq);
 bool analyse_instr(uint32_t raw_instr, instr_t* instr);
 void get_operands_i(uint32_t raw_instr, instr_t* instr);
 void get_operands_u(uint32_t raw_instr, instr_t* instr);
 void get_operands_r(uint32_t raw_instr, instr_t* instr);
 void get_operands_s(uint32_t raw_instr, instr_t* instr);
+
+//DFG
+dfg_t create_dfg(instr_seq_t* instr_seq);
+
 
 #endif //SEQSELECT_H
